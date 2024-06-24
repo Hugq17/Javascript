33cbc4f71a01aaf006a48e7a7b7181b438e173df
hung1720 <coderlazy17@gmail.com>
2024-06-24 08:46:43 +0700

    day1

---
+++ Day1/main.js
@@ -0,0 +1,28 @@
+/*
+    Một số hàm built-in được xây dựng sẵn trong JavaScript
+    1. Alert
+    2. Confirm
+    3. Prompt
+    4. console
+    5. setTimeout
+    6. setInterval
+*/
+
+// confirm("Are you sure ?");
+// prompt("What is your name ?");
+// setTimeout(function () {
+//   console.log("Please call me now");
+// }, 5000);
+
+// setInterval(function () {
+//   console.log("Sau 5s hay call me now");
+// }, 5000);
+
+//------------------------------------------------------------//
+/*
+    Giới thiệu về toán tử trong Javascript
+    1. toán tử số học - Arithmentic
+    2. Toán tử gán - Assignment
+    3. Toán tử so sánh - Comparison
+    4. Toán tử Logic - Logical
+*/
