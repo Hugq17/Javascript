094f131f455daae253abc51f29c7bcf0e9cff7d4
hung1720 <coderlazy17@gmail.com>
2024-06-24 11:57:52 +0700

    debounce

--- Tricks/Debounce.js
+++ Tricks/Debounce.js
@@ -2,9 +2,24 @@
 
 const initApp = () => {
   const button = document.querySelector("button");
-  button.addEventListener("click", clickOrder);
+  button.addEventListener("click", debounce(clickOrder, 4000));
 };
 
 const clickOrder = () => console.log("Order");
 
 document.addEventListener("DOMContentLoaded", initApp);
+
+const debounce = (fn, delay) => {
+  let timerid;
+  console.log(`timerid: ${timerid}`);
+  return () => {
+    console.warn(`timerid: ${timerid}`);
+    if (timerid) {
+      clearTimeout(timerid);
+      //   timerid = null;
+    }
+    timerid = setTimeout(function () {
+      fn();
+    }, delay);
+  };
+};
